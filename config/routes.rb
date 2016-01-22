90ce7e94475c49fb9137e98223514107e197ec4b
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-23 02:52:04 +0700

    Restrict auto-generated routes - only create and index for cars/car_types

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cars do
-    resources :car_types
+    resources :car_types, only: [:create, :index]
     post 'car_types_prices/:car_type_slug' => 'car_types#create'
   end
 end
