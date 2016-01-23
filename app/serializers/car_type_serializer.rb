3414df650025cb97b3d49f01cd0f889db9091f93
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-23 22:04:15 +0700

    Add schema_information to serializers

--- app/serializers/car_type_serializer.rb
+++ app/serializers/car_type_serializer.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: car_types
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  car_type_slug :string
+#  car_type_code :string
+#  base_price    :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  car_id        :integer
+#
+
 class CarTypeSerializer < ActiveModel::Serializer
-  attributes :name, :total_price
+  attributes :name, :total_price, :base_price
 end
