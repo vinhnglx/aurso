207a14a09ee6c5071786d2f9d528b58853bdc5d2
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-23 20:07:53 +0700

    Implement secure for POST car_type API

--- app/controllers/car_types_controller.rb
+++ app/controllers/car_types_controller.rb
@@ -13,8 +13,10 @@
 #
 
 class CarTypesController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   include ::Car::Finder
   include ::Car::Parameter
+  before_filter :restrict_access, only: :create
 
   def index
     cars = load_car(car_params[:car_slug])
@@ -29,4 +31,15 @@ class CarTypesController < ApplicationController
   def create
     head :created
   end
+
+  private
+
+    # Private: Require access token before create a car type
+    #
+    # Returns Boolean
+    def restrict_access
+      authenticate_or_request_with_http_token do |token, _options|
+        ApiToken.exists?(access_token: token)
+      end
+    end
 end
