c8d1c4c7ae0e43d2de13abf674b66083bb550882
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-23 19:56:38 +0700

    Remove validates access_token from ApiToken model Add callback generate_access_token for ApiToken model

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -9,6 +9,18 @@
 #
 
 class ApiToken < ActiveRecord::Base
-  # Validations
-  validates :access_token, presence: true
+  # Callbacks
+  before_create :generate_access_token
+
+  private
+
+    # Private: Genereate access token
+    #
+    # Return a Token
+    def generate_access_token
+      loop do
+        self.access_token = SecureRandom.hex
+        break unless self.class.exists?(access_token: access_token)
+      end
+    end
 end
