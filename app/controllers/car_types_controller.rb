67b9a6943ffc1d238638f4725bc3fc8992e48b2e
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-23 15:35:33 +0700

    Update routes for APIs

--- app/controllers/car_types_controller.rb
+++ app/controllers/car_types_controller.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: car_types
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  car_type_slug :string
+#  car_type_code :string
+#  base_price    :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  car_id        :integer
+#
+
 class CarTypesController < ApplicationController
   def index
     head :ok
--- spec/controllers/car_types_controller_spec.rb
+++ spec/controllers/car_types_controller_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: car_types
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  car_type_slug :string
+#  car_type_code :string
+#  base_price    :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  car_id        :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe CarTypesController, type: :controller do
@@ -8,7 +22,7 @@ RSpec.describe CarTypesController, type: :controller do
 
     describe 'car slug valid' do
       it 'returns list of cars types' do
-        get :index, car_id: 'hello-world'
+        get :index, car_slug: 'hello-world'
         expect(response.status).to eq 200
       end
     end
@@ -21,7 +35,7 @@ RSpec.describe CarTypesController, type: :controller do
 
     context 'car slug valid' do
       it 'returns new car type' do
-        post :create, car_id: 'hello-world'
+        post :create, car_slug: 'hello-world', car_type_slug: 'hello-world'
         expect(response.status).to eq 201
       end
     end
